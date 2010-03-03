3ec0b2b5c4b46798c8265667d03f2246f43d93d2
Subhan <subhan.py@gmail.com>
2010-03-03 06:43:48 -0600

    even functionality added

---
+++ even.erl
@@ -0,0 +1,5 @@
+-module(even).
+-export([even_list/2]).
+even_list(A,B) ->
+	lists:filter(fun(N) -> N rem 2 =:= 0 end,
+		mymisc:for(A,B,fun(X) -> X end)).
---
+++ math1.erl
@@ -0,0 +1,7 @@
+-module(math1).
+-export([double/1]).
+double(X) ->
+	times(X,2).
+
+times(X,N) ->
+	X * N.
