8f66bef7aed317890a78060d9447dc3811f51224
Subhan <subhan.py@gmail.com>
2010-03-04 00:58:15 -0600

    filter added in mymisc

--- mymisc.erl
+++ mymisc.erl
@@ -1,4 +1,11 @@
 -module(mymisc).
--export([for/3]).
+-export([for/3,filter/2]).
 for(Max,Max,F) -> [F(Max)];
 for(I,Max,F) -> [F(I)|for(I+1,Max,F)].
+
+filter(Pred, []) -> [];
+filter(Pred, [H|T]) ->
+	case Pred(H) of
+		true  -> [H | filter(Pred, T)];
+		false -> filter(Pred, T)
+	end.
